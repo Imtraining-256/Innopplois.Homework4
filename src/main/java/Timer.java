8c8468d25cd00328ba52a06c7301c52a563bdbf4
atenyaeves <atenyaev@protonmail.com>
2021-10-04 00:18:52 +0700

    Renamed variables

--- src/main/java/Timer.java
+++ src/main/java/Timer.java
@@ -5,12 +5,12 @@ public class Timer {
     public static void main(String[] args) {
         var chronometer = new Chronometer();
         var secondsPrinter = new Printer(chronometer, "", 1);
-        var Printer1 = new Printer(chronometer, "5 second left", 5);
-        var Printer2 = new Printer(chronometer, "7 seconds left", 7);
+        var notifyer5seconds = new Printer(chronometer, "5 second left", 5);
+        var notifyer7seconds = new Printer(chronometer, "7 seconds left", 7);
 
         new Thread(chronometer).start();
         new Thread(secondsPrinter).start();
-        new Thread(Printer1).start();
-        new Thread(Printer2).start();
+        new Thread(notifyer5seconds).start();
+        new Thread(notifyer7seconds).start();
     }
 }
